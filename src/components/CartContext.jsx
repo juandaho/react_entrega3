52b370fc7475d2c282f7a7a2e5cb0d5ccac49143
Juan David Holguin <juandaho@gmail.com>
2023-05-14 20:11:26 -0500

    Con Checkout

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,44 +1,41 @@
-import React from "react";
-import { createContext, useState } from "react";
+import React, { createContext, useState, useContext } from 'react';
 
 export const CartContext = createContext({
-    cart: [],
+  cart: [],
+  addItem: () => {},
+  removeItem: () => {},
+  clearCart: () => {},
 });
 
-
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
-    console.log(cart);
-    
-    const addItem = (item, quantity) => {
-        if (!isInCart(item.id)) {
-            setCart((prev) => [...prev, { ...item, quantity }]);
-        } else {
-            console.error("El item ya existe en el carrito");
-        }
-    };
+  const [cart, setCart] = useState([]);
 
-    const removeItem = (itemId) => {
-        const cartUpdated = cart.filter(prod => prod.id !== itemId);
-        setCart(cartUpdated);
+  const addItem = (item, quantity) => {
+    if (!isInCart(item.id)) {
+      setCart((prev) => [...prev, { ...item, quantity }]);
+    } else {
+      console.error('El item ya existe en el carrito');
     }
+  };
 
- const clearCart = () => {
+  const removeItem = (itemId) => {
+    const cartUpdated = cart.filter((prod) => prod.id !== itemId);
+    setCart(cartUpdated);
+  };
+
+  const clearCart = () => {
     setCart([]);
-    };
+  };
 
-    const isInCart = (itemId) => {
-        return cart.some((prod) => prod.id === itemId);
-    }
+  const isInCart = (itemId) => {
+    return cart.some((prod) => prod.id === itemId);
+  };
 
-    
+  return (
+    <CartContext.Provider value={{ cart, addItem, removeItem, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
 
-    return (
-        <CartContext.Provider value={{ cart, addItem, removeItem, clearCart }}>
-            {children}
-        </CartContext.Provider>
-    )
-
-}
-
-
+export const useCartContext = () => useContext(CartContext);
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { addDoc, collection, getDoc, doc, writeBatch, Timestamp } from 'firebase/firestore';
+import { db } from '../firebase/firebaseConfig';
+import { useCartContext } from './CartContext';
+import CheckoutForm from './CheckoutForm';
+
+import './Checkout.css';
 
 export const Checkout = () => {
+  const [loading, setLoading] = useState(false);
+  const [orderId, setOrderId] = useState('');
+  const { cart, total, clearCart } = useCartContext();
+
+  const createOrder = async ({ name, phone, email }) => {
+    setLoading(true);
+
+    try {
+      const objOrder = {
+        buyer: { name, phone, email },
+        items: cart,
+        date: Timestamp.fromDate(new Date()),
+        total: total,
+      };
+
+      const batch = writeBatch(db);
+      const outOfStock = [];
+      const ids = cart.map((prod) => prod.id);
+      const productsRef = collection(db, 'products');
+
+      const productsAddedFromFirebase = await Promise.all(
+        ids.map((id) => getDoc(doc(productsRef, id)))
+      );
+
+      productsAddedFromFirebase.forEach((doc, index) => {
+        if (doc.data().stock >= cart[index].quantity) {
+          batch.update(doc.ref, { stock: doc.data().stock - cart[index].quantity });
+        } else {
+          outOfStock.push({ ...doc.data(), id: doc.id });
+        }
+      });
+
+      if (outOfStock.length === 0) {
+        await batch.commit();
+      } else {
+        console.log('No hay stock de los siguientes productos: ', outOfStock);
+      }
+
+      const docRef = await addDoc(collection(db, 'orders'), objOrder);
+      setOrderId(docRef.id);
+      clearCart();
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <div className="loading">Se está generando su orden</div>;
+  }
+
+  if (orderId) {
+    return (
+      <div className="checkout">
+        <h2>Gracias por su compra!</h2>
+        <p>Su número de orden es: {orderId}</p>
+        <Link to="/">Volver al inicio</Link>
+      </div>
+    );
+  }
+
   return (
-    <div>Checkout</div>
-  )
-}
+    <div className="checkout">
+      <CheckoutForm onConfirm={createOrder} />
+    </div>
+  );
+};
