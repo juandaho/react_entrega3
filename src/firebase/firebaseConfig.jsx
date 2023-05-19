bbd26b88a3ee02d470fdad31ab43695a4ba81173
Juan David Holguin <juandaho@gmail.com>
2023-05-19 11:05:48 -0500

    Proyecto Finalizado con READEM.md

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-apiKey: process.env.REACT_APP_apiKey,
-authDomain: process.env.REACT_APP_authDomain,
-projectId: process.env.REACT_APP_projectId,
-storageBucket: process.env.REACT_APP_storageBucket,
-messagingSenderId: process.env.REACT_APP_messagingSenderId,
-appId: process.env.REACT_APP_appId,
-};
+    apiKey: "AIzaSyBRmA7uLkYC2pa9JHAq4ltPUjguY7R3Dao",
+    authDomain: "react-197f9.firebaseapp.com",
+    projectId: "react-197f9",
+    storageBucket: "react-197f9.appspot.com",
+    messagingSenderId: "975159165993",
+    appId: "1:975159165993:web:2f94fe70fb2db32b828838"
+  };
 
 
 
