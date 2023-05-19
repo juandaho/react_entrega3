e7e05706f6b504ff06dfbd12246c10d1ef892320
Juan David Holguin <juandaho@gmail.com>
2023-05-19 14:01:48 -0500

    Proyecto Finalizado con READEM.md

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -3,24 +3,17 @@ import { getFirestore } from "firebase/firestore";
 
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBRmA7uLkYC2pa9JHAq4ltPUjguY7R3Dao",
-//     authDomain: "react-197f9.firebaseapp.com",
-//     projectId: "react-197f9",
-//     storageBucket: "react-197f9.appspot.com",
-//     messagingSenderId: "975159165993",
-//     appId: "1:975159165993:web:2f94fe70fb2db32b828838"
-//   };
-
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
+    apiKey: "AIzaSyBRmA7uLkYC2pa9JHAq4ltPUjguY7R3Dao",
+    authDomain: "react-197f9.firebaseapp.com",
+    projectId: "react-197f9",
+    storageBucket: "react-197f9.appspot.com",
+    messagingSenderId: "975159165993",
+    appId: "1:975159165993:web:2f94fe70fb2db32b828838"
+  };
+
+
+
 
 
 const app = initializeApp(firebaseConfig);
